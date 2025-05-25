aac2ba11f396daf10cd860e76be5a49cffea214a
Sahil Sharma <148064634+Sahilsharma-ss@users.noreply.github.com>
2025-05-25 19:56:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import numpy as np
 import tensorflow as tf
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -17,5 +18,10 @@ def predict():
     label = "Anomaly" if prediction > 0.5 else "Normal"
     return jsonify({"result": label, "probability": float(prediction)})
 
+@app.route("/")
+def home():
+    return "LSTM IDS API is running!"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's dynamic port
+    app.run(host="0.0.0.0", port=port)
